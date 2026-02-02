8d19c086f1fb4a5a6dffeb9b7580989b03e9180c
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-02-02 20:17:26 +0530

    Some fixes in producer

--- producer.py
+++ producer.py
@@ -2,32 +2,34 @@ import uuid
 import json
 from confluent_kafka import Producer
 
-producer_config = Producer({'bootstrap.servers': 'localhost:9092'})
-
-producer = Producer(producer_config)
+producer = Producer({
+    'bootstrap.servers': 'localhost:9092'
+})
 
 def report_log(err, msg):
     if err:
-        print(f"Delivered Failed Error : {err}")
-        
+        print(f"Delivery failed: {err}")
     else:
-        print(f"Delivered Successfully : {msg.value().decode("utf-8")}")
-        print(f"Delivered Successfullt {msg.topic()}: Partition: {msg.partition()}: offset {msg.offset()}" )
-    
-    
-    
+        print(
+            f"Delivered to {msg.topic()} | "
+            f"Partition {msg.partition()} | "
+            f"Offset {msg.offset()}"
+        )
+
 order = {
-    "order__id": str(uuid.uuid4()),
+    "order_id": str(uuid.uuid4()),
     "user": "shiva",
     "item": "butter_chicken",
-    "quantiy": 4,
+    "quantity": 4,
 }
 
+value = json.dumps(order).encode("utf-8")
 
-value = json.dumps(order).encode("utf-8")  ## this line will convert into json to byte format cu'z kafka understand byte format
-
-producer.produce(topic=order, value=value, callback=report_log)
+producer.produce(
+    topic="orders",  
+    value=value,
+    callback=report_log
+)
 
 producer.flush()
-
 
